8fdd220e8574c6820052097760b135f3597bb7aa
Manasvi Gottipati <68877823+mgottipati@users.noreply.github.com>
2023-06-23 11:49:52 -0500

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿//using Microsoft.EntityFrameworkCore;
+
+namespace Students.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
+
